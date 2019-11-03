14627d109a3b6df3d3576201bd805ad3b4417923
SeungHyun Min <asdf014563@naver.com>
2019-11-03 11:24:20 +0900

    파일명 변경

--- ConsoleGame/ImageUtils/DefaultImageLayer.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "ImageLayer.h"
-#include "ImageLayerImpl.h"
-
-const ImageLayer DEFAULT_IMAGE_LAYER = { NULL, 0, NULL, NULL,_initialize, _renderAll};
--- ConsoleGame/ImageUtils/ImageLayer.h
+++ ConsoleGame/ImageUtils/ImageLayer.h
@@ -1,20 +1,5 @@
 #pragma once
-#include <Windows.h>
+#include "ImageLayerInterface.h"
+#include "ImageLayerImpl.h"
 
-typedef struct {
-	wchar_t* fileName;
-	int x, y;
-}Image;
-
-typedef struct _ImageLayer {
-	//멤버 변수
-	Image* images;
-	int imageCount;
-
-	HWND _windowHandle;
-	HDC _consoleDC;
-
-	//함수
-	void (*initialize)(struct _ImageLayer*);
-	void (*renderAll)(struct _ImageLayer*);
-}ImageLayer;
+const ImageLayer DEFAULT_IMAGE_LAYER = { NULL, 0, NULL, NULL,_initialize, _renderAll};
