9e9b419fb6e858d2b5507c1cf11b27c4af2fe07d
SeungHyun Min <asdf014563@naver.com>
2019-11-04 13:12:50 +0900

    Coord(int, int) 삭제

--- ConsoleGame/Utils.h
+++ ConsoleGame/Utils.h
@@ -7,8 +7,3 @@ HWND WINDOW_HANDLE;
 inline void gotoxy(COORD position) {
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
 }
-
-inline COORD Coord(int x, int y) {
-	const COORD result = { x, y };
-	return result;
-}
