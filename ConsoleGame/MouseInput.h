39cc598890b4de26ed85a2a8b6e90e087b4ae10c
SeungHyun Min <asdf014563@naver.com>
2019-11-04 11:10:58 +0900

    isMouseClicked() 알고리즘 변경

--- ConsoleGame/MouseInput.h
+++ ConsoleGame/MouseInput.h
@@ -3,37 +3,14 @@
 #include <Windows.h>
 #include "Utils.h"
 
-inline int hasInput() {
-	INPUT_RECORD input_record;
-	DWORD input_count;
-
-	PeekConsoleInput(CONSOLE_INPUT, &input_record, 1, &input_count);
-	return input_count;
-}
-
-inline int getInput(COORD* pos) {
-	INPUT_RECORD input_record;
-	DWORD input_count;
-
-	ReadConsoleInput(CONSOLE_INPUT, &input_record, 1, &input_count);
-	if (input_record.EventType == MOUSE_EVENT &&
-		input_record.Event.MouseEvent.dwButtonState & FROM_LEFT_1ST_BUTTON_PRESSED) {
-		CONSOLE_SCREEN_BUFFER_INFO csbi;
-
-		GetConsoleScreenBufferInfo(CONSOLE_OUTPUT, &csbi);
-
-		*pos = input_record.Event.MouseEvent.dwMousePosition;
-		pos->X -= csbi.srWindow.Left;
-		pos->Y -= csbi.srWindow.Top;
-
-		return MOUSE_EVENT;
-	}
-	return 0;
-}
-
-inline int isMouseClicked(COORD* position) {
-	if (hasInput() && getInput(position)) {
-		return 1;
+INPUT_RECORD rec;
+DWORD dwNOER;
+inline int isMouseClicked() {
+	ReadConsoleInput(GetStdHandle(STD_INPUT_HANDLE), &rec, 1, &dwNOER);
+	if (rec.EventType == MOUSE_EVENT) {
+		const DWORD buttonState = rec.Event.MouseEvent.dwButtonState;
+		if (buttonState & FROM_LEFT_1ST_BUTTON_PRESSED)	return 1;
+		else if (buttonState & RIGHTMOST_BUTTON_PRESSED) return 2;
 	}
 	return 0;
 }
@@ -42,5 +19,5 @@ inline COORD getMousePosition() {
 	POINT cursorPosition;
 	GetCursorPos(&cursorPosition);
 	ScreenToClient(WINDOW_HANDLE, &cursorPosition);
-	return Coord(cursorPosition.x * 2, cursorPosition.y);
+	return Coord(cursorPosition.x * 2, cursorPosition.y * 2);
 }
