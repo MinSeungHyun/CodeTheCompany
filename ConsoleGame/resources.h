a52583a319176b422d20f725f604682fc73652ca
SeungHyun Min <asdf014563@naver.com>
2019-11-06 19:43:47 +0900

    이미지 리소스 파일 추가

---
+++ ConsoleGame/resources.h
@@ -0,0 +1,6 @@
+#pragma once
+#define FILE_TITLE L"resources/background/title.bmp"
+#define FILE_TITLE_TEXT L"resources/text/title_text.bmp"
+#define FILE_START_BUTTON L"resources/button/start button.bmp"
+#define FILE_START_BUTTON_HOVER L"resources/button/start button_hover.bmp"
+#define FILE_START_BUTTON_CLICK L"resources/button/start button_clicked.bmp"
