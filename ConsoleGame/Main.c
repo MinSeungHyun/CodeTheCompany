7f0790b62631b6244304ebbbfa8ddb84c440ced1
SeungHyun Min <asdf014563@naver.com>
2019-11-06 11:17:00 +0900

    Revert "내용 일부 수정"
    This reverts commit 7d9d656e43c2434140634e10db6d9c4e2e0bf41f.

--- ConsoleGame/Main.c
+++ ConsoleGame/Main.c
@@ -1,25 +1,8 @@
 #include "Initializer.h"
-#include "ImageUtils/ImageLayer.h"
-#include "resources.h"
 
 int main() {
 	initialize();
-	Sleep(500);
-
-	ImageLayer layer = DEFAULT_IMAGE_LAYER;
-	layer.initialize(&layer);
-
-	Image images[2] = {
-		{FILE_STORY_1, 0, 0},
-		{FILE_STORY_2, 100, 200, 2}
-	};
-	layer.images = images;
-	layer.imageCount = 2;
-	layer.transparentColor = RGB(0, 255, 25);
-
-	layer.renderAll(&layer);
-
-	layer.fadeIn()
+	Sleep(300);
 
 	getchar();
 }
