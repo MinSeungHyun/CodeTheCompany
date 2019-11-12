308f8eda18e57ac884f2c02c31055c5871772964
SeungHyun Min <asdf014563@naver.com>
2019-11-12 22:08:07 +0900

    리소스 폴더 구조 변경

--- ConsoleGame/resources.h
+++ ConsoleGame/resources.h
@@ -1,10 +1,10 @@
 #pragma once
-#define FILE_TITLE "resources/background/title.bmp"
-#define FILE_TITLE_TEXT "resources/text/title_text.bmp"
-#define FILE_START_BUTTON "resources/button/start button.bmp"
-#define FILE_START_BUTTON_HOVER "resources/button/start button_hover.bmp"
-#define FILE_START_BUTTON_CLICK "resources/button/start button_clicked.bmp"
+#define FILE_TITLE "resources/start_screen/title.bmp"
+#define FILE_TITLE_TEXT "resources/start_screen/title_text.bmp"
+#define FILE_START_BUTTON "resources/start_screen/start_button.bmp"
+#define FILE_START_BUTTON_HOVER "resources/start_screen/start_button_hover.bmp"
+#define FILE_START_BUTTON_CLICK "resources/start_screen/start_button_clicked.bmp"
 
-#define FILE_STORY_1 "resources/background/story_1.bmp"
-#define FILE_STORY_2 "resources/background/story_2.bmp"
-#define FILE_STORY_3 "resources/background/story_3.bmp"
+#define FILE_STORY_1 "resources/story_screen/story_1.bmp"
+#define FILE_STORY_2 "resources/story_screen/story_2.bmp"
+#define FILE_STORY_3 "resources/story_screen/story_3.bmp"
