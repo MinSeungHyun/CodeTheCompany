4e5e4e3c73bbe3980b54d873461fa947e3ecd727
SeungHyun Min <asdf014563@naver.com>
2019-11-13 10:16:45 +0900

    trim 함수 추가

--- ConsoleGame/Utils.h
+++ ConsoleGame/Utils.h
@@ -7,3 +7,32 @@ HWND WINDOW_HANDLE;
 inline void gotoxy(COORD position) {
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
 }
+
+inline char* ltrim(char* str) {
+	const char* seps = "\t\n\v\f\r ";
+	const size_t totrim = strspn(str, seps);
+	if (totrim > 0) {
+		const size_t len = strlen(str);
+		if (totrim == len) {
+			str[0] = '\0';
+		}
+		else {
+			memmove(str, str + totrim, len + 1 - totrim);
+		}
+	}
+	return str;
+}
+
+inline char* rtrim(char* str) {
+	const char* seps = "\t\n\v\f\r ";
+	int i = strlen(str) - 1;
+	while (i >= 0 && strchr(seps, str[i]) != NULL) {
+		str[i] = '\0';
+		i--;
+	}
+	return str;
+}
+
+inline char* trim(char* str) {
+	return ltrim(rtrim(str));
+}
