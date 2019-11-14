6f50b93e3bba6b14c29f2a2109ea141e700e9c23
SeungHyun Min <asdf014563@naver.com>
2019-11-14 09:57:25 +0900

    companyName 저장, 불러오기 추가

--- ConsoleGame/SaveFileManager.h
+++ ConsoleGame/SaveFileManager.h
@@ -3,9 +3,11 @@
 
 #define DIR_SAVE "saves"
 #define DIR_NAME "saves/name.ctc"
+#define DIR_COMPANY_NAME "saves/company_name.ctc"
 
 inline void saveName(char* lastName, char* firstName) {
 	_mkdir(DIR_SAVE);
+
 	FILE* file = fopen(DIR_NAME, "w");
 	fprintf(file, "%s %s", lastName, firstName);
 	fclose(file);
@@ -15,5 +17,20 @@ inline int loadName(char* lastName, char* firstName) {
 	FILE* file = fopen(DIR_NAME, "r");
 	if (file == NULL) return 0;
 	fscanf(file, "%s %s", lastName, firstName);
+	fclose(file);
+	return 1;
+}
+
+inline void saveCompanyName(char* companyName) {
+	FILE* file = fopen(DIR_COMPANY_NAME, "w");
+	fprintf(file, "%s", companyName);
+	fclose(file);
+}
+
+inline int loadCompanyName(char* companyName) {
+	FILE* file = fopen(DIR_COMPANY_NAME, "r");
+	if (file == NULL) return 0;
+	fscanf(file, "%s", companyName);
+	fclose(file);
 	return 1;
 }
